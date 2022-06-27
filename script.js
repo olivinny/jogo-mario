1e1180fe16d06a42c7d53069f1272ee2a7864c30
olivinny <vinicius.gtoliv@gmail.com>
2022-06-27 00:50:53 -0300

    restart apenas

--- script.js
+++ script.js
@@ -5,44 +5,41 @@ const start = document.querySelector(".start");
 const restart = document.querySelector(".restart");
 const reload = () => {
   location.reload();
-};
-function startGame() {
   start.classList.remove("active");
-  restart.classList.remove("active");
+};
+start.classList.remove("active");
+restart.classList.remove("active");
 
-  const jump = () => {
-    mario.classList.add("jump");
-    setTimeout(() => {
-      mario.classList.remove("jump");
-    }, 500);
-  };
+const jump = () => {
+  mario.classList.add("jump");
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
 
-  const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window
-      .getComputedStyle(mario)
-      .bottom.replace("px", "");
-    const cloudsPosition = clouds.offsetLeft;
-    if (pipePosition <= 120 && marioPosition < 100 && pipePosition > 0) {
-      pipe.style.animation = "none";
-      pipe.style.left = `${pipePosition}px`;
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+  const cloudsPosition = clouds.offsetLeft;
+  if (pipePosition <= 120 && marioPosition < 100 && pipePosition > 0) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-      mario.style.animation = "none";
-      mario.style.bottom = `${marioPosition}px`;
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
 
-      clouds.style.animation = "none";
-      clouds.style.left = `${cloudsPosition}px`;
+    clouds.style.animation = "none";
+    clouds.style.left = `${cloudsPosition}px`;
 
-      mario.src = "./assets/img/game-over.png";
-      mario.style.width = "75px";
-      mario.style.marginLeft = "50px";
-      clearInterval(loop);
-      restart.classList.add("active");
-      restart.addEventListener("click", reload);
-    }
-  }, 10);
+    mario.src = "./assets/img/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
+    clearInterval(loop);
+    restart.classList.add("active");
+    restart.addEventListener("click", reload);
+  }
+}, 10);
 
-  document.addEventListener("keydown", jump);
-}
-
-start.addEventListener("click", startGame);
+document.addEventListener("keydown", jump);
